c94d8e5472f10a629e6d7db4d0b6a0e38607fb08
James Ng'ang'a <james.ng'ang'a@student.moringaschool.com>
2022-11-15 17:01:15 +0300

    ft: create industry and segment tables

--- revenue/models.py
+++ revenue/models.py
@@ -15,3 +15,18 @@ class FiscalCountry(models.Model):
 
     def __str__(self):
         return self.country_name
+
+class Industry(models.Model):
+    industry_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.industry_name
+
+class Segment(models.Model):
+    segment_name = models.CharField(max_length=50)
+    industry = models.ForeignKey(Industry, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.segment_name
+
+        
