4141dc34980283759cb079b02528cd0c84a86a06
James Njoroge <98014792+devjamesnjoroge@users.noreply.github.com>
2022-11-23 12:36:02 +0300

    ft: clean up

--- revenue/models.py
+++ revenue/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Region(models.Model):
-    RegionName = models.CharField(max_length=45)
+    RegionName = models.CharField(max_length=45, unique=True)
 
     def __str__(self):
         return self.RegionName
